a3693cd055886748395fa30fc25f12256d10c480
Bryce Crist <38509232+qlibcrist@users.noreply.github.com>
2020-12-25 18:56:09 -0700

    Update Chip8.hpp

--- Chip8.hpp
+++ Chip8.hpp
@@ -1,22 +1,30 @@
+#ifndef UNTITLED_CHIP8_H
+#define UNTITLED_CHIP8_H
+
 #include <cstdint>
 
 class Chip8 {
-    
-    public:
-        // Member Variables:
-        uint8_t registers[16]{};
-        uint8_t memory[4096]{};
-        uint16_t index{};
-        uint16_t pc{};
-        uint16_t stack[16]{};
-        uint8_t stackPointer{};
-        uint8_t delayTimer{};
-        uint8_t sountTimerP{};
-        uint8_t keypad[16]{};
-        uint32_t video[16 * 32]{};
-        uint16_t opcode;
 
-        // Methods:
-        void loadRom(char const* fileName);
+public:
+    // Constructor:
+    Chip8();
+
+    // Member Variables:
+    uint8_t registers[16]{};
+    uint8_t memory[4096]{};
+    uint16_t index{};
+    uint16_t programCounter{};
+    uint16_t stack[16]{};
+    uint8_t stackPointer{};
+    uint8_t delayTimer{};
+    uint8_t soundTimer{};
+    uint8_t keypad[16]{};
+    uint32_t video[16 * 32]{};
+    uint16_t opcode;
+
+    // Methods:
+    void loadRom(char const* fileName);
 
 };
+
+#endif
